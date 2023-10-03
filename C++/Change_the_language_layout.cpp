0e41e5d7224dd787942660bf2ae010a1a8d3ad48
flyupunity <flyupworking@gmail.com>
2023-10-03 12:29:36 +0700

    Add project
    Milanote:
    https://app.milanote.com/1QNxnz1OOakz2q?p=nlt2x7VVK9z
    
    12:24 03.10.2023

---
+++ C++/Change_the_language_layout.cpp
@@ -0,0 +1,37 @@
+﻿#include <Windows.h>
+#include <setupapi.h>
+#include <iostream>
+
+int main() {
+    // Enumerate all input devices
+    HDEVINFO deviceInfoSet = SetupDiGetClassDevs(&GUID_DEVCLASS_HIDCLASS, 0, 0, DIGCF_PRESENT | DIGCF_DEVICEINTERFACE);
+
+    if (deviceInfoSet == INVALID_HANDLE_VALUE) {
+        std::cerr << "Error enumerating devices." << std::endl;
+        return 1;
+    }
+
+    // Iterate through devices and identify keyboards
+    SP_DEVINFO_DATA deviceInfoData;
+    deviceInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
+
+    // TODO: Identify keyboards based on device information
+
+    // Capture and process input events
+    RAWINPUTDEVICE rawInputDevice[1];
+    rawInputDevice[0].usUsagePage = 1; // Generic Desktop Controls
+    rawInputDevice[0].usUsage = 6;     // Keyboard
+    rawInputDevice[0].dwFlags = RIDEV_INPUTSINK;
+    rawInputDevice[0].hwndTarget = NULL;
+
+    if (!RegisterRawInputDevices(rawInputDevice, 1, sizeof(RAWINPUTDEVICE))) {
+        std::cerr << "Error registering raw input devices." << std::endl;
+        return 1;
+    }
+
+    while (true) {
+        // TODO: Capture and process input events, and determine the source keyboard
+    }
+
+    return 0;
+}
